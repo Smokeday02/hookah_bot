01160a5be2f9e1e775d2db50f4a8941daffaf82c
Smokeday02 <ansarbekturganov69@gmail.com>
2026-03-08 18:27:42 +0600

    021 bot.py

--- bot.py
+++ bot.py
@@ -39,7 +39,7 @@ conn.commit()
 # =======================
 # Функции для работы с базой
 # =======================
-def create_order(user_id, pack):
+async def create_order(user_id, pack):
     cursor.execute(
         "INSERT INTO orders (user_id, pack, status) VALUES (?, ?, ?)",
         (user_id, pack, 'active')
@@ -139,10 +139,11 @@ async def save_pack(message: types.Message):
 
     await message.answer(text)
     user_id = message.from_user.id
+    pack = message.text
     if user_id not in orders:
         orders[user_id] = {}
     orders[user_id]["pack"] = message.text
-    create_order(user_id, pack)
+    await create_order(user_id, pack)
     await message.answer("Комплект сохранён ✅", reply_markup=menu)
 
 @dp.message_handler(lambda m: m.text in ["Средний - 13000"])
@@ -165,10 +166,11 @@ async def save_pack(message: types.Message):
 
     await message.answer(text)
     user_id = message.from_user.id
+    pack = message.text
     if user_id not in orders:
         orders[user_id] = {}
     orders[user_id]["pack"] = message.text
-    create_order(user_id, pack)
+    await create_order(user_id, pack)
     await message.answer("Комплект сохранён ✅", reply_markup=menu)
 
 @dp.message_handler(lambda m: m.text in ["Крепкий - 16000"])
@@ -191,10 +193,11 @@ async def save_pack(message: types.Message):
 
     await message.answer(text)
     user_id = message.from_user.id
+    pack = message.text
     if user_id not in orders:
         orders[user_id] = {}
     orders[user_id]["pack"] = message.text
-    create_order(user_id, pack)
+    await create_order(user_id, pack)
     await message.answer("Комплект сохранён ✅", reply_markup=menu)
 
 # основной номер
@@ -467,6 +470,7 @@ async def finish_order_callback(callback: types.CallbackQuery):
 # запуск бота
 
 executor.start_polling(dp)
+
 
 
 
